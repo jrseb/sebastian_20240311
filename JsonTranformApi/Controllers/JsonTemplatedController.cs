e2c7382a03bdef12dd9f2328066efdf5be29f0ce
Guillermo Sebastian <jhune.sebastian@gmail.com>
2024-03-11 08:04:17 +0800

    Code push but no update on readme yet

---
+++ JsonTranformApi/Controllers/JsonTemplatedController.cs
@@ -0,0 +1,50 @@
+using JsonTransformLibrary.interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Immutable;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace JsonTranformApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class JsonTemplatedController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<JsonTemplatedController> _logger;
+		public readonly ITransform _transform;
+
+		public JsonTemplatedController(ILogger<JsonTemplatedController> logger, ITransform transformsvc)
+		{
+			_logger = logger;
+			_transform = transformsvc;
+		}
+
+		/// <summary>
+		/// Tranform a Json array to a templated output
+		/// </summary>
+		/// <param name="jsonRecords">json records</param>
+		/// <param name="template">template format to use</param>
+		/// <returns>string</returns>
+		[HttpPost(Name = "TransformJson")]
+		public IActionResult ConvertToTemplate([FromBody] object[] jsonRecords, string template)
+		{
+			var recs = jsonRecords.ToImmutableList();
+			List<JsonElement> items = new();
+			recs.ForEach(rec => items.Add((JsonElement)rec));
+
+			if (_transform.ValidateParameters(items.ToImmutableList(), template))
+			{
+				var result = _transform.JsonToTemplate(items.ToImmutableList(), template, 10);
+				return Ok(result);
+			}
+			else
+				return NotFound("Template/Tags or json element is empty");
+		}
+	}
+}
---
+++ JsonTranformApi/ReadMe.md
@@ -0,0 +1,13 @@
+# JSON To Template API
+## About the Project
+This project is the Web API component of the JsonTranform Solution.  Compose of a single controller injected with tranformation component.  This can be run under docker linux distro and has no database dependency.
+
+The API require both the template parameter and a JSON array in order to work properly. Otherwise respose will show error messages.  Please check the swagger for further information.
+
+## How to build and Run 
+A docker file was created to 
+
+##design decisions.
+
+## About The Author
+Name: ** Basti Sebastian **
